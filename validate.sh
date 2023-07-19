771cd7debdef5e230dcb7069ece6c8235ea768f2
Dan Checkoway <dcheckoway@gmail.com>
2023-07-19 11:47:37 -0600

    Add script to make validation trivial

---
+++ validate.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash -e
+
+PLUGIN_ID=$(grep '"id"' < src/plugin.json | sed -E 's/.*"id" *: *"(.*)".*/\1/')
+echo "Validating the ${PLUGIN_ID} plugin"
+
+if [ ! -d dist ]; then
+  echo "Building the plugin first"
+  yarn build
+fi
+
+cleanup () {
+  rm -rf ${PLUGIN_ID} ${PLUGIN_ID}.zip
+}
+trap cleanup EXIT
+
+
+echo "Copying and zipping"
+cleanup # in case stuff was left around from last time
+cp -r dist ${PLUGIN_ID}
+zip -qr ${PLUGIN_ID}.zip ${PLUGIN_ID}
+
+echo "Invoking the plugin validator"
+npx @grafana/plugin-validator@latest -sourceCodeUri file://. ${PLUGIN_ID}.zip
