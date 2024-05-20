5d4afed11e77d079062f8f1f048e784ed38e5ce9
Dan Checkoway <dcheckoway@gmail.com>
2024-05-20 09:41:30 -0400

    pr feedback: useMemo (also useCallback)

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react';
+import React, { ChangeEvent, KeyboardEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { InlineField, Select, TextArea } from '@grafana/ui';
 import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import { CriblDataSource } from '../datasource';
@@ -18,7 +18,7 @@ export function QueryEditor({ datasource, query, onChange, onRunQuery }: Props) 
   const [queryType, setQueryType] = useState(query.type as string ?? DEFAULT_QUERY_TYPE);
   const [adhocQuery, setAdhocQuery] = useState(query.type === 'adhoc' ? query.query : '');
 
-  const onQueryTypeChange = (sv: SelectableValue<string>) => {
+  const onQueryTypeChange = useCallback((sv: SelectableValue<string>) => {
     const newQueryType = sv.value ?? DEFAULT_QUERY_TYPE;
     setQueryType(newQueryType);
     if (newQueryType === 'saved') {
@@ -26,32 +26,32 @@ export function QueryEditor({ datasource, query, onChange, onRunQuery }: Props) 
     } else {
       onChange({ ...query, type: 'adhoc', query: adhocQuery });
     }
-  };
+  }, [adhocQuery, onChange, query, savedSearchId]);
 
-  const onAdhocQueryChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+  const onAdhocQueryChange = useCallback((event: ChangeEvent<HTMLTextAreaElement>) => {
     const newQuery = event.target.value;
     setAdhocQuery(newQuery);
     onChange({ ...query, type: 'adhoc', query: newQuery });
     // Don't run it automatically, the user can hit Enter or click "Run query" when ready
-  };
+  }, [onChange, query]);
 
-  const onAdhocQueryKeyDown = (event: KeyboardEvent) => {
+  const onAdhocQueryKeyDown = useCallback((event: KeyboardEvent) => {
     if (event.key === 'Enter' && !event.shiftKey) { // allow shift-enter to add a line break
       event.preventDefault();
       if (adhocQuery.trim().length > 0) {
         onRunQuery();
       }
     }
-  };
+  }, [adhocQuery, onRunQuery]);
 
-  const onSavedQueryIdChange = (sv: SelectableValue<string>) => {
+  const onSavedQueryIdChange = useCallback((sv: SelectableValue<string>) => {
     const newSavedSearchId = sv.value?.replace(/\s+/g, '') ?? ''; // auto-trim/remove any whitespace
     setSavedSearchId(newSavedSearchId);
     if (newSavedSearchId !== savedSearchId && newSavedSearchId.match(/^[a-zA-Z0-9_]+$/)) {
       onChange({ ...query, type: 'saved', savedSearchId: newSavedSearchId });
       debouncedOnRunQuery();
     }
-  };
+  }, [debouncedOnRunQuery, onChange, query, savedSearchId]);
 
   // Load the saved search IDs, let the user just pick one
   useEffect(() => {
@@ -69,7 +69,7 @@ export function QueryEditor({ datasource, query, onChange, onRunQuery }: Props) 
     loadSavedSearchIds();
   }, [datasource]);
 
-  const getQueryUI = () => {
+  const QueryFields = useMemo(() => {
     if (queryType === 'saved') {
       return (
         <InlineField label="Saved Search" labelWidth={16} tooltip="ID of the Cribl saved search">
@@ -91,14 +91,14 @@ export function QueryEditor({ datasource, query, onChange, onRunQuery }: Props) 
         </InlineField>
       );
     }
-  };
+  }, [adhocQuery, onAdhocQueryChange, onAdhocQueryKeyDown, onSavedQueryIdChange, queryType, savedSearchId, savedSearchIdOptions]);
 
   return (
     <div className="gf-form">
       <InlineField label="Query Type" labelWidth={12}>
         <Select onChange={onQueryTypeChange} options={QUERY_TYPE_OPTIONS} value={queryType} width={12} />
       </InlineField>
-      {getQueryUI()}
+      {QueryFields}
     </div>
   );
 }
