d72f1d042b595ed8a05fde7def1aa936c3c48a32
Dan Checkoway <dcheckoway@gmail.com>
2024-11-01 13:42:01 -0400

    update the validator to including signing

--- validate.sh
+++ validate.sh
@@ -8,16 +8,22 @@ rm -rf dist
 npm run build
 mage -v
 
+if [ ! -z "$GRAFANA_ACCESS_POLICY_TOKEN" ]; then
+  npx @grafana/sign-plugin@latest
+else
+  echo "Skipping signing the plugin, set GRAFANA_ACCESS_POLICY_TOKEN to include signing"
+fi
+
 cleanup () {
   rm -rf ${PLUGIN_ID} ${PLUGIN_ID}.zip
 }
 trap cleanup EXIT
-
 
 echo "Copying and zipping"
 cleanup # in case stuff was left around from last time
 cp -r dist ${PLUGIN_ID}
 zip -qr ${PLUGIN_ID}.zip ${PLUGIN_ID}
 
-echo "Invoking the plugin validator"
+echo "Invoking the plugin validator (no output, no warnings, no errors is a good thing)"
 npx @grafana/plugin-validator@latest -sourceCodeUri file://. ${PLUGIN_ID}.zip
+echo "Done"
