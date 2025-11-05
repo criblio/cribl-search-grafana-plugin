a8e4718a977529c6f1909e9861f604f28d6d7dac
Dan Checkoway <dcheckoway@gmail.com>
2025-11-05 10:50:34 -0500

    SEARCH-11518 Use buildinfo to get plugin version for User-Agent

--- pkg/main.go
+++ pkg/main.go
@@ -9,12 +9,6 @@ import (
 )
 
 func main() {
-	// TODO: ideally we'd be able to glean this from package.json.
-	// I played around with defining this at build time via ldflags, but that took us down
-	// a path where if we modify build scripts, we'd deviate from easy upgradeability in the
-	// future.  I have an open thread with Grafana community Slack to find a better way.
-	plugin.SetVersion("1.1.4")
-
 	// Start listening to requests sent from Grafana. This call is blocking so
 	// it won't finish until Grafana shuts down the process or the plugin choose
 	// to exit by itself using os.Exit. Manage automatically manages life cycle
